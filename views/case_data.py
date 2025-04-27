568bf09a0aefc37af8b8083d9495546d7a78431f
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2025-04-27 22:21:30 +0600

    Editing feature for Case list page added

--- views/case_data.py
+++ views/case_data.py
@@ -5,6 +5,49 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 from models.component_selection_manager import ComponentSelectionManager
 
 
+class EditDialog(QtWidgets.QDialog):
+    def __init__(self, id_, name, size, price, parent=None):
+        super(EditDialog, self).__init__(parent)
+        self.setWindowTitle("Edit Case")
+        self.resize(300, 200)
+
+        self.id = id_
+
+        layout = QtWidgets.QVBoxLayout()
+
+        self.name_input = QtWidgets.QLineEdit(name)
+        self.size_input = QtWidgets.QLineEdit(size)
+        self.price_input = QtWidgets.QLineEdit(price)
+
+        layout.addWidget(QtWidgets.QLabel("Name:"))
+        layout.addWidget(self.name_input)
+        layout.addWidget(QtWidgets.QLabel("Size:"))
+        layout.addWidget(self.size_input)
+        layout.addWidget(QtWidgets.QLabel("Price:"))
+        layout.addWidget(self.price_input)
+
+        self.save_btn = QtWidgets.QPushButton("Save")
+        self.save_btn.clicked.connect(self.save_changes)
+        layout.addWidget(self.save_btn)
+
+        self.setLayout(layout)
+
+    def save_changes(self):
+        new_name = self.name_input.text()
+        new_size = self.size_input.text()
+        new_price = self.price_input.text()
+
+        connection = sqlite3.connect("data/database/database.sqlite")
+        cursor = connection.cursor()
+        cursor.execute("""
+            UPDATE Cases SET Name = ?, Size = ?, Price = ? WHERE id = ?
+        """, (new_name, new_size, new_price, self.id))
+        connection.commit()
+        connection.close()
+
+        self.accept()
+
+
 class Ui_CasePage(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -34,7 +77,6 @@ class Ui_CasePage(object):
         self.tabWidget.setStyleSheet("font: 16pt 'Arial';")
         self.tabWidget.setObjectName("tabWidget")
 
-        # Tab 1 - Case Details
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
 
@@ -42,40 +84,34 @@ class Ui_CasePage(object):
 
         self.tabWidget.addTab(self.tab, "Case Details")
 
-        # Tab 2 - Edit Details (currently empty)
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
         self.tabWidget.addTab(self.tab_2, "Edit Details")
 
     def setup_case_details_tab(self):
-        # Table
         self.table = QtWidgets.QTableWidget(self.tab)
         self.table.setGeometry(QtCore.QRect(10, 110, 1231, 381))
         self.table.setStyleSheet("font: 10pt 'Arial';")
         self.table.setObjectName("table")
-        self.table.setColumnCount(5)
+        self.table.setColumnCount(6)
         self.table.setRowCount(0)
-        headers = ["id", "Name", "Size", "Price", "Action"]
+        headers = ["id", "Name", "Size", "Price", "Add", "Edit"]
         self.table.setHorizontalHeaderLabels(headers)
 
-        # Label
         self.label_2 = QtWidgets.QLabel(self.tab)
         self.label_2.setGeometry(QtCore.QRect(10, 10, 500, 41))
         self.label_2.setStyleSheet("font: 20pt 'Arial';")
         self.label_2.setObjectName("label_2")
 
-        # SpinBox for Count Filtering
         self.count_filter_txt = QtWidgets.QSpinBox(self.tab)
         self.count_filter_txt.setGeometry(QtCore.QRect(520, 10, 111, 41))
         self.count_filter_txt.setObjectName("count_filter_txt")
 
-        # Button for Count-based Search
         self.search_btn = QtWidgets.QPushButton(self.tab)
         self.search_btn.setGeometry(QtCore.QRect(650, 10, 151, 41))
         self.search_btn.setObjectName("search_btn")
         self.search_btn.clicked.connect(self.load_case_data)
 
-        # Search Bar for Keyword Search
         self.keyword_search_input = QtWidgets.QLineEdit(self.tab)
         self.keyword_search_input.setGeometry(QtCore.QRect(820, 10, 261, 41))
         self.keyword_search_input.setPlaceholderText("Search by Name...")
@@ -88,20 +124,17 @@ class Ui_CasePage(object):
         self.keyword_search_btn.clicked.connect(self.search_by_keyword)
 
     def setup_footer(self, MainWindow):
-        # Refresh Button
         self.refresh_btn = QtWidgets.QPushButton(self.centralwidget)
         self.refresh_btn.setGeometry(QtCore.QRect(1120, 670, 141, 31))
         self.refresh_btn.setStyleSheet("font: 14pt 'Arial';")
         self.refresh_btn.setObjectName("refresh_btn")
         self.refresh_btn.clicked.connect(self.load_case_data)
 
-        # Back Button
         self.back_btn = QtWidgets.QPushButton(self.centralwidget)
         self.back_btn.setGeometry(QtCore.QRect(30, 670, 141, 31))
         self.back_btn.setStyleSheet("font: 14pt 'Arial';")
         self.back_btn.setObjectName("back_btn")
 
-        # Menubar and Statusbar
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1299, 26))
         self.menubar.setObjectName("menubar")
@@ -140,27 +173,22 @@ class Ui_CasePage(object):
     def search_by_keyword(self):
         keyword = self.keyword_search_input.text().strip()
 
-        if not keyword:
-            connection = sqlite3.connect("data/database/database.sqlite")
-            cursor = connection.cursor()
-
-            cursor.execute("SELECT * FROM Cases")
-            rows = cursor.fetchall()
-            self.populate_table(rows)
-            connection.close()
-            return
-
         connection = sqlite3.connect("data/database/database.sqlite")
         cursor = connection.cursor()
 
-        cursor.execute("SELECT * FROM Cases WHERE Name LIKE ? OR Size LIKE ? OR Price LIKE ? OR id LIKE ?", ('%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%'))
+        if keyword:
+            cursor.execute("""
+                SELECT * FROM Cases WHERE Name LIKE ? OR Size LIKE ? OR Price LIKE ? OR id LIKE ?
+            """, (f"%{keyword}%", f"%{keyword}%", f"%{keyword}%", f"%{keyword}%"))
+        else:
+            cursor.execute("SELECT * FROM Cases")
+
         rows = cursor.fetchall()
         self.populate_table(rows)
         connection.close()
 
     def populate_table(self, rows):
         self.table.setRowCount(len(rows))
-        self.table.setColumnCount(5)
 
         for row_num, row_data in enumerate(rows):
             for col_num, data in enumerate(row_data[:4]):
@@ -171,10 +199,25 @@ class Ui_CasePage(object):
             add_button.clicked.connect(lambda _, r=row_num: self.handle_add_button(r))
             self.table.setCellWidget(row_num, 4, add_button)
 
+            edit_button = QtWidgets.QPushButton("Edit")
+            edit_button.setStyleSheet("font-family: Arial;")
+            edit_button.clicked.connect(lambda _, r=row_num: self.handle_edit_button(r))
+            self.table.setCellWidget(row_num, 5, edit_button)
+
     def handle_add_button(self, row):
         case_name = self.table.item(row, 1).text()
         self.manager.set_component_name("Case", case_name)
         print(f"'Add' button clicked for Case Name: {case_name}")
+
+    def handle_edit_button(self, row):
+        id_ = self.table.item(row, 0).text()
+        name = self.table.item(row, 1).text()
+        size = self.table.item(row, 2).text()
+        price = self.table.item(row, 3).text()
+
+        dialog = EditDialog(id_, name, size, price)
+        if dialog.exec():
+            self.load_case_data()
 
 
 class CasePage(QtWidgets.QMainWindow):
